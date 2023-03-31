9d7525abd54063ddeff07316f318245cb0570ca8
DESKTOP-2P2I6TL\Lee Then Huynh <Binbinhung2112@gmail.com>
2023-03-31 15:42:10 +0700

    cm

---
+++ Angular15/src/app/component/component.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-component',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './component.component.html',
+  styleUrls: ['./component.component.scss']
+})
+export class ComponentComponent {
+
+}
